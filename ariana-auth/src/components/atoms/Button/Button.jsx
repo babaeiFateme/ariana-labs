e98919af4d4158e813ffec575e67f0bac15faf72
fatemeh <babaei.f1996@gmail.com>
2025-05-12 20:20:41 +0330

    feat: create button component

---
+++ ariana-auth/src/components/atoms/Button/Button.jsx
@@ -0,0 +1,19 @@
+import cn from "../../../core/utils/cn/cn.utils";
+import buttonStyle from "./Button.constants";
+
+const Button = ({ variant = "base", className, children, ...props }) => {
+    return (
+        <button
+            {...props}
+            className={cn(
+                "w-full px-2 py-4 font-medium text-sm leading-6 rounded-md",
+                className,
+                buttonStyle.variants[variant]
+            )}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
---
+++ ariana-auth/src/components/atoms/Button/button.constants.js
@@ -0,0 +1,12 @@
+const variants = {
+    primary: "bg-primary text-white",
+    secondary: "bg-secondary text-secondary-foreground",
+    base: "bg-white text-secondary-foreground",
+    destructive: "bg-destructive text-white",
+};
+
+const buttonStyle = {
+    variants,
+};
+
+export default buttonStyle;
