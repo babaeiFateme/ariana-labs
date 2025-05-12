fa12d45fb69fa2afb5bca9d5082c028f422b4c29
fatemeh <babaei.f1996@gmail.com>
2025-05-13 01:01:35 +0330

    fix: add name for input

--- ariana-auth/src/components/organisms/LoginOrganisms/Form/LoginForm.jsx
+++ ariana-auth/src/components/organisms/LoginOrganisms/Form/LoginForm.jsx
@@ -8,32 +8,47 @@ import API_ENDPOINTS from "../../../../core/services/constants/routes.constants"
 
 const LoginForm = () => {
     const { fetchData, data, isLoading, isError, errorMessage } = useFetch();
-    const [form, setForm] = useState({ email: "", password: "" });
-  
+    const [form, setForm] = useState({ username: "", password: "" });
+
     const handleChange = (e) => {
-      setForm({ ...form, [e.target.name]: e.target.value });
+        setForm({ ...form, [e.target.name]: e.target.value });
     };
-  
+
     const handleSubmit = (e) => {
-      e.preventDefault();
-      fetchData({
-        url: API_ENDPOINTS.login,
-        method: "POST",
-        data: form,
-      });
+        e.preventDefault();
+        fetchData({
+            url: API_ENDPOINTS.login,
+            method: "POST",
+            data: form,
+        });
     };
     return (
         <form className="flex flex-col gap-y-4" onSubmit={handleSubmit}>
             <Field name={"Username"}>
-                <TextInput placeholder="Please enter your username" />
+                <TextInput
+                    placeholder="Please enter your username"
+                    value={form.username}
+                    onChange={handleChange}
+                    name="username"
+                />
             </Field>
 
             <Field name={"Password"}>
-                <PasswordInput placeholder="Please enter your password" />
+                <PasswordInput
+                    placeholder="Please enter your password"
+                    value={form.password}
+                    onChange={handleChange}
+                    name="password"
+                />
             </Field>
 
-            <Button variant="primary" className={"py-4"} type="submit" disabled={isLoading}>
-            {isLoading ? "Logging in..." : "Login"}
+            <Button
+                variant="primary"
+                className={"py-4"}
+                type="submit"
+                disabled={isLoading}
+            >
+                {isLoading ? "Logging in..." : "Login"}
             </Button>
         </form>
     );
