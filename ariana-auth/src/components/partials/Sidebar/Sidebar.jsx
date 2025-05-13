537ea658a495570879776478203126226e69321f
fatemeh <babaei.f1996@gmail.com>
2025-05-13 05:43:09 +0330

    feat: make user info

--- ariana-auth/src/components/partials/Sidebar/Sidebar.jsx
+++ ariana-auth/src/components/partials/Sidebar/Sidebar.jsx
@@ -1,15 +1,31 @@
 import Button from "../../atoms/Button/Button";
 import Arrow from "../../icons/Arrow";
-
+import user from "../../../../public/images/pages/dashboard/user.jpg";
 
 const Sidebar = () => {
     return (
         <aside className="bg-[#F8FAFC] w-[240px] border border-[#E2E8F0] flex flex-col justify-between h-screen px-2 py-6">
-            <div className=" mb-10 mt-10">
-                <img src="" alt="" />
+            <div className=" mb-10 mt-10 text-center">
+                <img
+                    src={user}
+                    alt="user"
+                    className="w-16 aspect-square rounded-full mx-auto"
+                />
+
+                <div className="font-bold text-['15px'] leading-6 text-black mt-2">
+                    Shahab Hosseini
+                </div>
+
+                <div className="font-normal text-[15px] leading-6 text-[#585858]">
+                    @ShahabH
+                </div>
             </div>
 
-            <Button variant="destructive" icon={<Arrow />} className="flex gap-1.5 items-center justify-center">
+            <Button
+                variant="destructive"
+                icon={<Arrow />}
+                className="flex gap-1.5 items-center justify-center"
+            >
                 Logout
             </Button>
         </aside>
