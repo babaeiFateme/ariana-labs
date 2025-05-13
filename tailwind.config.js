37e5a75a8acd42d15ac32f13d8e909af496814e4
fatemeh <babaei.f1996@gmail.com>
2025-05-13 11:08:52 +0330

    chore: remove extra directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import COLOR_PALETTES from './src/config/colors/color-palette' 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: { ...COLOR_PALETTES },
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#0F172A",
+                'secondary-foreground': "##0F172A",
+                secondary: "#F1F5F9",
+                destructive: "#DC2626",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
