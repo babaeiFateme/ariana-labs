09909a482955308e913f67fa50971f6cc797fe5f
fatemeh <babaei.f1996@gmail.com>
2025-05-14 10:57:08 +0330

    feat: create empty state component

---
+++ src/components/atoms/EmptyState/EmptyState.jsx
@@ -0,0 +1,20 @@
+import cn from "../../../core/utils/cn/cn.utils";
+
+const EmptyState = ({ img, imgClassName, children }) => {
+    return (
+        <div className="flex gap-1 items-center justify-center flex-col">
+            <img
+                className={cn(
+                    "max-w-[280px] aspect-square object-cover",
+                    imgClassName
+                )}
+                src={img}
+                alt="empty state"
+            />
+
+            <div className=""> {children}</div>
+        </div>
+    );
+};
+
+export default EmptyState;
