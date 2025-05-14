ebff79ccea95dd40684051f5f148d23a6b54132c
fatemeh <babaei.f1996@gmail.com>
2025-05-14 16:01:27 +0330

    wip: create skeleton component

---
+++ src/components/atoms/Skeleton/Skeleton.jsx
@@ -0,0 +1,5 @@
+const Skeleton = () => {
+    return <div className="min-h-[60px] min-w-full w-full bg-gray-300 rounded-md flex justify-center items-center mt-6"> loading ...</div>;
+};
+
+export default Skeleton;
