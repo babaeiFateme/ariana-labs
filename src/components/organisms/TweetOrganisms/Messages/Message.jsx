b4ebe0c06a392e8921ca86d8c1696b1cac996b00
fatemeh <babaei.f1996@gmail.com>
2025-05-14 16:02:42 +0330

    refactor: api connected for tweet message

--- src/components/organisms/TweetOrganisms/Messages/Message.jsx
+++ src/components/organisms/TweetOrganisms/Messages/Message.jsx
@@ -1,35 +1,60 @@
-import img from "../../../../../public/images/pages/tweet/user.png";
+import { useEffect, useState } from "react";
+import useFetch from "../../../../core/hooks/useFetch";
+import API_ENDPOINTS from "../../../../core/services/constants/routes.constants";
 import EmptyState from "../../../atoms/EmptyState/EmptyState";
-import messageInfo from "./resources/message";
+import TimeHandler from "../../../../core/helpers/TimeHandler";
+import Skeleton from "../../../atoms/Skeleton/Skeleton";
+
 const Message = () => {
-    const isEmpty = messageInfo.length === 0;
+    const { fetchData, isLoading, isError } = useFetch();
+    const [data, useData] = useState(null);
+
+    useEffect(() => {
+        fetchData({
+            url: API_ENDPOINTS.tweet_create,
+            method: "GET",
+            headers: {
+                Authorization: `Token ${localStorage.getItem("token")}`,
+                "X-CSRFToken": localStorage.getItem("token") ?? "",
+            },
+            onSuccess: (result) => {
+                useData(result.results);
+            },
+            onError: (error) => {
+                console.error("Failed to fetch messages:", error.message);
+            },
+        });
+    }, []);
+
+    const isEmpty = !data || data.length === 0;
+
+    if (isLoading) return <Skeleton />;
+    if (isError) return <div>Error loading messages.</div>;
 
     return (
         <>
             {!isEmpty ? (
                 <ul className="*:bg-gray-650 *:mb-4 *:p-4 mt-4 *:rounded-md">
-                    {messageInfo.map((tweet) => (
+                    {data.map((tweet) => (
                         <li key={tweet.id}>
                             <div className="flex gap-2.5 items-center flex-wrap">
                                 <img
-                                    src={img}
+                                    src="/images/pages/tweet/user.png"
                                     alt="user"
                                     className="w-8 aspect-square rounded-full"
                                 />
-
                                 <div className="flex flex-col items-start">
                                     <span className="text-black font-medium text-md leading-3 mb-1">
-                                        {tweet.username}
+                                        {tweet.author.first_name}
+                                        {tweet.author.lname_name}
                                     </span>
-
                                     <span className="text-gray-900 text-sm font-light">
-                                        30 minutes ago
+                                        {TimeHandler(tweet.created_at)}
                                     </span>
                                 </div>
                             </div>
-
                             <div className="mt-[13px] min-h-[61px] overflow-auto font-light font-sm">
-                                {tweet.message}
+                                {tweet.text}
                             </div>
                         </li>
                     ))}
@@ -37,9 +62,7 @@ const Message = () => {
             ) : (
                 <EmptyState>
                     <div className="text-medium font-thin">
-                        No results found for
-                        <span className="font-bold">“React”</span>. Try checking
-                        your spelling or using different keywords.
+                        No messages found. Try again later.
                     </div>
                 </EmptyState>
             )}
---
+++ src/core/helpers/TimeHandler.js
@@ -0,0 +1,23 @@
+function TimeHandler(isoString) {
+    const inputDate = new Date(isoString);
+    const now = new Date();
+  
+    const diffInMs = now - inputDate;
+    const diffInSeconds = Math.floor(diffInMs / 1000);
+  
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds} seconds ago`;
+    } else if (diffInSeconds < 3600) {
+      const minutes = Math.floor(diffInSeconds / 60);
+      return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+    } else if (diffInSeconds < 86400) {
+      const hours = Math.floor(diffInSeconds / 3600);
+      return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+    } else {
+      const days = Math.floor(diffInSeconds / 86400);
+      return `${days} day${days !== 1 ? "s" : ""} ago`;
+    }
+  }
+  
+  export default TimeHandler;
+  
