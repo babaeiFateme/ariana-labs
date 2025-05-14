19f71cfbb93294f66cb318957bc69fa7b94522c2
fatemeh <babaei.f1996@gmail.com>
2025-05-14 13:13:27 +0330

    refactor: refactor tweet form

--- src/components/organisms/TweetOrganisms/Form/TweetForm.jsx
+++ src/components/organisms/TweetOrganisms/Form/TweetForm.jsx
@@ -4,6 +4,7 @@ import Search from "../../../icons/Search";
 import Field from "../../../molecule/Field/Field";
 
 import img from "../../../../../public/images/pages/tweet/user.png";
+import Button from "../../../atoms/Button/Button";
 const TweetForm = () => {
     return (
         <>
@@ -21,16 +22,29 @@ const TweetForm = () => {
             </Field>
 
             <form>
-                <div className="border border-gray-200 rounded-md p-4 mt-[22px] relative flex gap-2 items-start">
-                    <img src={img} alt="user" className="w-10 aspect-square rounded-full"/>
-                    <Field name="message">
-                        <TextAreaInput
-                            rows="5"
-                            className="border-0 mt-0"
-                            placeholder="What ‘s Happening ?"
-                            name="message"
+                <div className="border border-gray-200 rounded-md p-4 mt-[22px]">
+                    <div className="relative flex gap-2 items-start">
+                        <img
+                            src={img}
+                            alt="user"
+                            className="w-10 aspect-square rounded-full"
                         />
-                    </Field>
+                        <Field name="message">
+                            <TextAreaInput
+                                rows="5"
+                                className="border-0 mt-1"
+                                placeholder="What ‘s Happening ?"
+                                name="message"
+                            />
+                        </Field>
+                    </div>
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="w-fit block mr-0 ml-auto py-2 !px-[25px]"
+                    >
+                        Post
+                    </Button>
                 </div>
             </form>
         </>
